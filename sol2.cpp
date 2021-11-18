d0a89eaacf3b975b473c903a7b6395da023fec00
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-11-18 22:07:19 +0800

    Update sol2.cpp

--- sol2.cpp
+++ sol2.cpp
@@ -1,1 +1,26 @@
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	string s;
+	cin >> s;
+	int n = 10;
+	map<string, char> mp;
+	for (int i = 0; i < n; i++) {
+		string digit;
+		cin >> digit;
+		// store the corresponding equivalent digit of the string
+		mp[digit] = (char) 48 + i;
+	}
+	string ans = "";
+	for (int i = 0; i < (int) s.size(); i += 10) {
+		// get the equivaelent digit of every 10-character substring
+		ans += (char) mp[s.substr(i, 10)];
+	}
+	cout << ans << '\n';
+	return 0;
+}
